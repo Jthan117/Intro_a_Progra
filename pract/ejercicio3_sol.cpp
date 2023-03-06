85dcfa83bfa8c331df5ac542f6bec08c925ccbd4
Jthan117 <126267040+Jthan117@users.noreply.github.com>
2023-03-06 09:51:21 -0400

    Add files via upload
    añadì algunos ejercicios

---
+++ pract/ejercicio3_sol.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int width, height;
+	cout << "ingrese el ancho y alto del rectangulo " << endl;
+	cin >> width >> height;
+	cout << "el area del rectangulo es "<< width * height << endl;
+
+	
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
---
+++ pract/ejercicio4_sol.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int x, y, z = 1;
+	cin >> x >> y;
+	cout << "your answer: ";
+	cout << x + y;
+
+	
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
